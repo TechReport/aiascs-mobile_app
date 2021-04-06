624f688527e5643ed4ad2b035e4f8cef6516af52
Kijacode <kijadanford@gmail.com>
2021-04-06 19:32:41 +0300

    add app pop-up_menu models

---
+++ lib/models/Service_Pop_up_menu.dart
@@ -0,0 +1,34 @@
+
+
+import 'package:flutter/material.dart';
+
+class ServicePopUpMenu {
+  String id;
+  String name;
+  String translatedName;
+  Color color;
+  FontWeight fontWeight;
+
+  ServicePopUpMenu({
+    this.id,
+    this.name,
+    this.translatedName,
+    this.color = const Color(0xFF1A3518),
+    this.fontWeight = FontWeight.normal,
+  });
+
+
+
+ static List<ServicePopUpMenu>
+      getDefaultServicePopUpMenus() {
+    return [
+      ServicePopUpMenu(id: 'logout', name: 'Logout'),
+      ServicePopUpMenu(id: 'sync', name: 'Sync'),
+      ServicePopUpMenu(id: 'language_setting', name: 'Language Settings'),
+      ServicePopUpMenu(id: 'settings', name: 'Settings'),
+      ServicePopUpMenu(id: 'report', name: 'Report'),
+      ServicePopUpMenu(id: 'profile', name: 'Profiles'),
+    ];
+  }
+
+}
