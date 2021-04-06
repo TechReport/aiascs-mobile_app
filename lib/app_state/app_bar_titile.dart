130bda5de4ffefbb9abc4872723b86ad3eccff97
Kijacode <kijadanford@gmail.com>
2021-04-06 19:30:50 +0300

    feat: add app-bar_title model

---
+++ lib/app_state/app_bar_titile.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/foundation.dart';
+
+class AppBarTitleState extends ChangeNotifier {
+//initstate
+  String _appBarTitle;
+  int _currenntSelectedBottomBar;
+
+  String get appBarTitle => _appBarTitle ?? "Home";
+  int get currentSelectedBottomBar => _currenntSelectedBottomBar ?? 0;
+
+//reducer
+  void setCurrentAppBarTitle(String currentAppBarTitle) {
+    _appBarTitle = currentAppBarTitle;
+    notifyListeners();
+  }
+
+  void setCurrentSelectedBottomBarIndex(int selectedIndex) {
+    _currenntSelectedBottomBar = selectedIndex;
+    notifyListeners();
+  }
+}
