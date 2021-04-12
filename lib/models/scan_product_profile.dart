d71e34faea939aa6c5cab1f27eb794ab0b3c939c
Kijacode <kijadanford@gmail.com>
2021-04-13 01:18:51 +0300

    feat: add product report profile on scan status

---
+++ lib/models/scan_product_profile.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/foundation.dart';
+
+class ScanProductProfile {
+  int productId;
+  String profileTitle;
+  String profileValue;
+
+  ScanProductProfile(
+      {@required this.productId,
+      @required this.profileTitle,
+      @required this.profileValue});
+
+  factory ScanProductProfile.fromJson(Map<String, dynamic> json) {
+    return ScanProductProfile(
+        productId: json[''], profileTitle: json[''], profileValue: json['']);
+  }
+
+  Map toOffline(ScanProductProfile scanProductProfile) {
+    var map = Map<String, dynamic>();
+    map[''] = scanProductProfile.productId;
+    map[''] = scanProductProfile.profileTitle;
+    map[''] = scanProductProfile.profileValue;
+    return map;
+  }
+
+  ScanProductProfile.fromOffline(Map<String, dynamic> map) {
+    this.productId = map[''];
+    this.profileTitle = map[''];
+    this.profileValue = map[''];
+  }
+
+ static List<ScanProductProfile> defaultProductProfileData() {
+    return [
+      ScanProductProfile(productId: 1,profileTitle: "Product Name",profileValue:" HDGTV-DAJFAK" ),
+       ScanProductProfile(productId: 2,profileTitle: "Product Id",profileValue: "yifoad-4098-vdfj-jdlkf"),
+        ScanProductProfile(productId: 3,profileTitle: "Manufacture Name",profileValue: "Seedco Company Limited"),
+         ScanProductProfile(productId: 4,profileTitle: "Manufacture Date",profileValue: "12-06-2005"),
+          ScanProductProfile(productId: 5,profileTitle: "Expire Date",profileValue: "15-06-2027"),
+           ScanProductProfile(productId: 6,profileTitle: "isRevoked",profileValue: "true"),
+            ScanProductProfile(productId: 7,profileTitle: "Country",profileValue: "Tanzania"),
+             ScanProductProfile(productId: 8,profileTitle: "State",profileValue: "Dar es salaam"),
+              ScanProductProfile(productId: 9,profileTitle: "support",profileValue: "GEN-profile"),
+    ];
+  }
+}
--- lib/modules/scan_qr_code/scan_product_report.dart
+++ lib/modules/scan_qr_code/scan_product_report.dart
@@ -1,6 +1,7 @@
 import 'package:aiascs_mobile/app_state/app_bar_titile.dart';
 import 'package:aiascs_mobile/core/components/enter_token_button.dart';
 import 'package:aiascs_mobile/core/components/spacer_component.dart';
+import 'package:aiascs_mobile/models/scan_product_profile.dart';
 import 'package:aiascs_mobile/modules/home/components/bottom_navigation_bar_options.dart';
 import 'package:aiascs_mobile/modules/scan_qr_code/components/qr_code_header.dart';
 import 'package:aiascs_mobile/modules/scan_qr_code/scan_Qr_Code.dart';
@@ -44,127 +45,105 @@ class _ScanProductReportState extends State<ScanProductReport> {
         elevation: 0,
         backgroundColor: Colors.white,
         leading: IconButton(
-          icon: Icon(Icons.arrow_back_ios,
-      color:  Color(0xFF9FB9CC)
-          ),
+          icon: Icon(Icons.arrow_back_ios, color: Color(0xFF9FB9CC)),
           onPressed: () {
-            Navigator.pushAndRemoveUntil(context, MaterialPageRoute (builder: (context) => new ScanQrCode()), (route) => false);
-
+            Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(builder: (context) => new ScanQrCode()),
+                (route) => false);
           },
         ),
       ),
-      body: ExpansionTile(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-
-                Text("FAKE PRODUCT",
+      body: SingleChildScrollView(
+              child: ExpansionTile(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text(
+                "FAKE PRODUCT",
                 style: TextStyle(
                   color: Colors.red,
                   fontWeight: FontWeight.bold,
                   fontSize: 27,
                 ),
-                
-                ),
-
-
-            Icon(
-              Icons.cancel_presentation_sharp,
-              size: 32,
-              color: Color(0xFF9FB9CC),
+              ),
+              Icon(
+                Icons.cancel_presentation_sharp,
+                size: 32,
+                color: Color(0xFF9FB9CC),
+              ),
+            ],
+          ),
+          children: [
+            QRCodeHeader(),
+            SingleChildScrollView(
+                        child: Container(
+                padding: EdgeInsets.symmetric(vertical: 2.0, horizontal: 10.0),
+                height: MediaQuery.of(context).size.height/2+1,
+                width: MediaQuery.of(context).size.width,
+                child:
+                    ListView.builder(
+                        scrollDirection: Axis.vertical,
+                        padding: EdgeInsets.symmetric(vertical: 30, horizontal: 10),
+                        itemCount:ScanProductProfile.defaultProductProfileData().length,
+                        itemBuilder: (context, int profileIndex) {
+                         
+                          return Container(
+                            color: Colors.grey[100],
+                            padding: EdgeInsets.symmetric(vertical: 6),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Expanded(
+                                  flex: 4,
+                                  child: Text(
+                                    ScanProductProfile.defaultProductProfileData()[
+                                            profileIndex]
+                                        .profileTitle,
+                                    style: TextStyle().copyWith(
+                                        fontSize: 14.0,
+                                        fontWeight: FontWeight.w500,
+                                        color: Colors.black),
+                                  ),
+                                ),
+                                Expanded(
+                                  flex: 2,
+                                  child: Text(
+                                    ":",
+                                    style: TextStyle().copyWith(
+                                      fontSize: 12.0,
+                                      fontWeight: FontWeight.normal,
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  flex: 4,
+                                  child: Text(
+                                    ScanProductProfile.defaultProductProfileData()[
+                                            profileIndex]
+                                        .profileValue,
+                                    style: TextStyle().copyWith(
+                                      fontSize: 14.0,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        }),
+                  
+              ),
             ),
-        
+            SpacerComponent(height: MediaQuery.of(context).size.height / 53),
+            EnterTokenButton(
+              width: MediaQuery.of(context).size.width / 2,
+              height: MediaQuery.of(context).size.height / 16,
+              title: "Revoke Product",
+              onPressButton: () {},
+            )
           ],
         ),
-        children: [
-          QRCodeHeader(),
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 5.0),
-            padding: EdgeInsets.symmetric(vertical: 2.0, horizontal: 10.0),
-            child: Column(children: [
-              Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Expanded(
-                  flex: 4,
-                  child: Text(
-                    "Company Name",
-                    style: TextStyle().copyWith(
-                        fontSize: 12.0,
-                        fontWeight: FontWeight.w500,
-                        color: Colors.black),
-                  ),
-                ),
-                Expanded(
-                  flex: 2,
-                  child: Text(
-                    ":",
-                    style: TextStyle().copyWith(
-                      fontSize: 12.0,
-                      fontWeight: FontWeight.normal,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  flex: 4,
-                  child: Text(
-                    "online",
-                    style: TextStyle().copyWith(
-                      fontSize: 14.0,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ),
-              
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Expanded(
-                  flex: 4,
-                  child: Text(
-                    "Company Name",
-                    style: TextStyle().copyWith(
-                        fontSize: 12.0,
-                        fontWeight: FontWeight.w500,
-                        color: Colors.black),
-                  ),
-                ),
-                Expanded(
-                  flex: 2,
-                  child: Text(
-                    ":",
-                    style: TextStyle().copyWith(
-                      fontSize: 12.0,
-                      fontWeight: FontWeight.normal,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  flex: 4,
-                  child: Text(
-                    "online",
-                    style: TextStyle().copyWith(
-                      fontSize: 14.0,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ),
-              
-              ],
-            ),
-          
-            ],),
-          ),
-           SpacerComponent(height: MediaQuery.of(context).size.height/4),
-          EnterTokenButton(
-                    width: MediaQuery.of(context).size.width / 2,
-                    height: MediaQuery.of(context).size.height / 16,
-                    title: "Revoke Product",
-                    onPressButton: (){},
-                  )
-        ],
       ),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Color(0xFF9FB9CC),
