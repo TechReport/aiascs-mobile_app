8dd17a17d4a794c12a6db31bdbdf054176d8d16d
Kijacode <kijadanford@gmail.com>
2021-04-22 14:25:30 +0300

    feat: intergrate auth with state

--- lib/app_state/Login_state.dart
+++ lib/app_state/Login_state.dart
@@ -6,20 +6,27 @@ class LoginState extends ChangeNotifier {
   bool _isLogin;
   String _loginMessage;
   bool _isAuth;
+  bool _isLoading;
+  bool _isLogout;
 
-  bool get isLogin => _isLogin;
+  bool get isLogin => _isLogin ?? false;
   String get loginMessage => _loginMessage ?? "";
   bool get isAuth => _isAuth ?? false;
+  bool get isLoading => _isLoading ?? false;
+  bool get isLogout => _isLogout ?? false;
 
   void setLogin(String email, String password) async {
-    _isLogin = true;
+    _isLoading = true;
+    notifyListeners();
     bool loginStatus = await LoginService().login(email, password);
     if (loginStatus) {
       _loginMessage = "You have Successfully Login";
-      _isLogin = false;
+      _isLoading = false;
+      _isLogin = true;
       notifyListeners();
     } else {
       _loginMessage = "Repeat the authentication process again";
+      _isLoading = false;
       _isLogin = false;
       notifyListeners();
     }
@@ -30,6 +37,28 @@ class LoginState extends ChangeNotifier {
     if (tokenValue.length > 0) {
       _isAuth = true;
       notifyListeners();
-    }    
+    }
+  }
+
+  void changePassword(String password) async {
+    _isLoading = true;
+    notifyListeners();
+    if (await LoginService().changePassword(password)) {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  void logout() async {
+    _isLoading = true;
+    if (await LoginService().logout()) {
+      _isLogout = true;
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  void clearStates() {
+    _isLoading = false;
   }
 }
