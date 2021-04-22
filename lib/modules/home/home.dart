e0c8e161b58b70d6f81d28373743769bb91989f5
Kijacode <kijadanford@gmail.com>
2021-04-22 14:28:23 +0300

    feat:add state utils for user + auth

--- lib/modules/home/home.dart
+++ lib/modules/home/home.dart
@@ -1,9 +1,12 @@
+import 'package:aiascs_mobile/app_state/Login_state.dart';
 import 'package:aiascs_mobile/app_state/app_bar_titile.dart';
 import 'package:aiascs_mobile/core/components/pop_up_menu_component.dart';
 import 'package:aiascs_mobile/core/utils/app_util.dart';
+import 'package:aiascs_mobile/modules/authentication/change_password/Change_Password.dart';
 import 'package:aiascs_mobile/modules/home/components/bottom_navigation_bar_options.dart';
 import 'package:aiascs_mobile/modules/scan_qr_code/scan_Qr_Code.dart';
 import 'package:aiascs_mobile/modules/services_module/Service_Home.dart';
+import 'package:aiascs_mobile/modules/splashscreen/splashscreen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -25,28 +28,28 @@ class _HomeState extends State<Home> {
 
     switch (_selectedindex) {
       case 3:
-       Provider.of<AppBarTitleState>(context, listen: false)
+        Provider.of<AppBarTitleState>(context, listen: false)
             .setCurrentAppBarTitle("Searching");
-      Provider.of<AppBarTitleState>(context,listen: false)
-      .setCurrentSelectedBottomBarIndex(3);
+        Provider.of<AppBarTitleState>(context, listen: false)
+            .setCurrentSelectedBottomBarIndex(3);
         break;
       case 1:
-       Provider.of<AppBarTitleState>(context, listen: false)
+        Provider.of<AppBarTitleState>(context, listen: false)
             .setCurrentAppBarTitle("Scan QR code");
-             Provider.of<AppBarTitleState>(context,listen: false)
-      .setCurrentSelectedBottomBarIndex(1);
+        Provider.of<AppBarTitleState>(context, listen: false)
+            .setCurrentSelectedBottomBarIndex(1);
         break;
       case 2:
-       Provider.of<AppBarTitleState>(context, listen: false)
+        Provider.of<AppBarTitleState>(context, listen: false)
             .setCurrentAppBarTitle("UnAouthirised Product");
-             Provider.of<AppBarTitleState>(context,listen: false)
-      .setCurrentSelectedBottomBarIndex(2);
+        Provider.of<AppBarTitleState>(context, listen: false)
+            .setCurrentSelectedBottomBarIndex(2);
         break;
       default:
         Provider.of<AppBarTitleState>(context, listen: false)
             .setCurrentAppBarTitle("Home");
-             Provider.of<AppBarTitleState>(context,listen: false)
-      .setCurrentSelectedBottomBarIndex(0);
+        Provider.of<AppBarTitleState>(context, listen: false)
+            .setCurrentSelectedBottomBarIndex(0);
     }
   }
 
@@ -58,46 +61,71 @@ class _HomeState extends State<Home> {
     Text("4"),
   ];
 
-  void onOpenMoreMenu()async{
-   var response = await  AppUtil.showPopUpModal(context, PopUpMenuComponent(), false);
-   print (response);
+  void onOpenMoreMenu() async {
+    String response =
+        await AppUtil.showPopUpModal(context, PopUpMenuComponent(), false);
+    switch (response) {
+      case "logout":
+        Provider.of<LoginState>(context, listen: false).clearStates();
+        Provider.of<LoginState>(context, listen: false).logout();
+        Navigator.push(
+            context, new MaterialPageRoute(builder: (context) => new Home()));
+        Navigator.pushAndRemoveUntil(context,new MaterialPageRoute(builder: (context) => new SplashScreen()) , (route) => false);
+        break;
+      case "password":
+        Navigator.push(context,
+            new MaterialPageRoute(builder: (context) => new ChangePassword()));
+
+        break;
+      default:
+        print(response);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        title: Text(
-            Provider.of<AppBarTitleState>(context, listen: false).appBarTitle),
-        backgroundColor: Color(0xFF9FB9CC),
-        actions: [
-          GestureDetector(
-            child: Icon(Icons.more_vert),
-            onTap:() => onOpenMoreMenu() ,
-          )
-        ],
-      ),
-      body: _widgetOptions.elementAt(_selectedindex),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Color(0xFF9FB9CC),
-        selectedIconTheme: IconThemeData(
-          color: Color(0xfff2f2f2),
+    return Consumer<LoginState>(
+        builder: (BuildContext context, loginState, child) {
+      return Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          title: Text(Provider.of<AppBarTitleState>(context, listen: false)
+              .appBarTitle),
+          backgroundColor: Color(0xFF9FB9CC),
+          actions: [
+            GestureDetector(
+              child: Icon(Icons.more_vert),
+              onTap: () => onOpenMoreMenu(),
+            )
+          ],
         ),
-        unselectedIconTheme: IconThemeData(color: Colors.white),
-        selectedLabelStyle: TextStyle(
-          color: Colors.white,
+        body: loginState.isLoading
+            ? Center(
+                child: Container(
+                  child: CircularProgressIndicator(),
+                ),
+              )
+            : _widgetOptions.elementAt(_selectedindex),
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Color(0xFF9FB9CC),
+          selectedIconTheme: IconThemeData(
+            color: Color(0xfff2f2f2),
+          ),
+          unselectedIconTheme: IconThemeData(color: Colors.white),
+          selectedLabelStyle: TextStyle(
+            color: Colors.white,
+          ),
+          showSelectedLabels: true,
+          showUnselectedLabels: true,
+          unselectedLabelStyle:
+              TextStyle(color: Color(0xfff2f2f2).withOpacity(0.5)),
+          fixedColor: Colors.yellow,
+          type: BottomNavigationBarType.fixed,
+          items: BottomNavigationOptions.navigationOptions,
+          currentIndex: _selectedindex,
+          onTap: _update_page,
         ),
-        showSelectedLabels: true,
-        showUnselectedLabels: true,
-        unselectedLabelStyle:
-            TextStyle(color: Color(0xfff2f2f2).withOpacity(0.5)),
-        fixedColor: Colors.yellow,
-        type: BottomNavigationBarType.fixed,
-        items: BottomNavigationOptions.navigationOptions,
-        currentIndex: _selectedindex,
-        onTap: _update_page,
-      ),
-    );
+      );
+    });
   }
 }
--- lib/modules/services_module/component/info_top_bar.dart
+++ lib/modules/services_module/component/info_top_bar.dart
@@ -1,5 +1,8 @@
+import 'package:aiascs_mobile/app_state/User_state.dart';
 import 'package:aiascs_mobile/core/components/Line_separator.dart';
+import 'package:aiascs_mobile/models/User.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class InfoTopHeader extends StatelessWidget {
   const InfoTopHeader({
@@ -8,108 +11,113 @@ class InfoTopHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Material(
-          type: MaterialType.card,
-          elevation: 1.0,
-          child: Container(
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-              child: Column(
-                children: [
-                  Container(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Expanded(
-                          child: Container(
-                            margin: EdgeInsets.symmetric(
-                                horizontal: 10.0, vertical: 5.0),
-                            child: Text(
-                              "Name",
-                              style: TextStyle().copyWith(
-                                  color: Color(0xFF1A3518),
-                                  fontSize: 14.0,
-                                  fontWeight: FontWeight.w700),
-                            ),
+    return Consumer<UserState>(
+        builder: (BuildContext context, userState, child) {
+      print("on home login");
+      print(userState.currentUser.firstname);
+     User user = userState.currentUser ;
+      return Material(
+        type: MaterialType.card,
+        elevation: 1.0,
+        child: Container(
+            margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+            child: Column(
+              children: [
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        child: Container(
+                          margin: EdgeInsets.symmetric(
+                              horizontal: 10.0, vertical: 5.0),
+                          child: Text(
+                            "Name",
+                            style: TextStyle().copyWith(
+                                color: Color(0xFF1A3518),
+                                fontSize: 14.0,
+                                fontWeight: FontWeight.w700),
                           ),
                         ),
-                        Expanded(
-                          child: Container(
-                            alignment: Alignment.centerRight,
-                            margin: EdgeInsets.symmetric(
-                                horizontal: 10.0, vertical: 5.0),
-                            child: Text(
-                              "Danford Kija David",
-                              style: TextStyle().copyWith(
-                                  color: Color(0xFF1A3518),
-                                  fontSize: 14.0,
-                                  fontWeight: FontWeight.w700),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(top: 5.0),
-                    child: LineSeperator(color: Color(0XFFECF5EC)),
-                  ),
-                  Container(
-                      margin: EdgeInsets.symmetric(
-                        horizontal: 10.0,
-                        vertical: 10.0,
                       ),
-                      child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Expanded(
-                              child: RichText(
-                                text: TextSpan(
-                                  text: 'Field ',
-                                  style: TextStyle().copyWith(
-                                    fontSize: 12.0,
-                                    fontWeight: FontWeight.w500,
-                                    color: Color(0xFF92A791),
-                                  ),
-                                  children: [
-                                    TextSpan(
-                                      text: "Morogoro",
-                                      style: TextStyle().copyWith(
-                                        fontSize: 12.0,
-                                        fontWeight: FontWeight.w500,
-                                        color: Color(0xFF9FB9CC),
-                                      ),
-                                    )
-                                  ],
+                      Expanded(
+                        child: Container(
+                          alignment: Alignment.centerRight,
+                          margin: EdgeInsets.symmetric(
+                              horizontal: 10.0, vertical: 5.0),
+                          child: Text(
+                          user.firstname ?? "" +" "+ user.lastname ?? ""  ,
+                            style: TextStyle().copyWith(
+                                color: Color(0xFF1A3518),
+                                fontSize: 14.0,
+                                fontWeight: FontWeight.w700),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 5.0),
+                  child: LineSeperator(color: Color(0XFFECF5EC)),
+                ),
+                Container(
+                    margin: EdgeInsets.symmetric(
+                      horizontal: 10.0,
+                      vertical: 10.0,
+                    ),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            child: RichText(
+                              text: TextSpan(
+                                text: 'email ',
+                                style: TextStyle().copyWith(
+                                  fontSize: 12.0,
+                                  fontWeight: FontWeight.w500,
+                                  color: Color(0xFF92A791),
                                 ),
+                                children: [
+                                  TextSpan(
+                                    text: user.email ?? "",
+                                    style: TextStyle().copyWith(
+                                      fontSize: 12.0,
+                                      fontWeight: FontWeight.w500,
+                                      color: Color(0xFF9FB9CC),
+                                    ),
+                                  )
+                                ],
                               ),
                             ),
-                            Expanded(
-                              flex: 2,
-                              child: RichText(
-                                text: TextSpan(
-                                  text: 'Status ',
-                                  style: TextStyle().copyWith(
-                                    fontSize: 12.0,
-                                    fontWeight: FontWeight.w500,
-                                    color: Color(0xFF92A791),
-                                  ),
-                                  children: [
-                                    TextSpan(
-                                      text: "Quality Controller",
-                                      style: TextStyle().copyWith(
-                                        fontSize: 12.0,
-                                        fontWeight: FontWeight.w500,
-                                        color: Color(0xFF9FB9CC),
-                                      ),
-                                    )
-                                  ],
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: RichText(
+                              text: TextSpan(
+                                text: 'Status ',
+                                style: TextStyle().copyWith(
+                                  fontSize: 12.0,
+                                  fontWeight: FontWeight.w500,
+                                  color: Color(0xFF92A791),
                                 ),
+                                children: [
+                                  TextSpan(
+                                    text: "Quality Controller",
+                                    style: TextStyle().copyWith(
+                                      fontSize: 12.0,
+                                      fontWeight: FontWeight.w500,
+                                      color: Color(0xFF9FB9CC),
+                                    ),
+                                  )
+                                ],
                               ),
                             ),
-                          ]))
-                ],
-              )),
-        
-    );
+                          ),
+                        ]))
+              ],
+            )),
+      );
+    });
   }
 }
