2f705a8665ea1066e8513c68bf49619f81f466c6
Kijacode <kijadanford@gmail.com>
2021-04-25 18:07:30 +0300

    feat: add service transaction model id

--- lib/models/transaction_service_card_model.dart
+++ lib/models/transaction_service_card_model.dart
@@ -2,57 +2,31 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class TransactionServiceCardModel {
+  final String id;
   final String title;
   final dynamic iconPath;
 
-  TransactionServiceCardModel({ @required  this.title, this.iconPath = ""});
+  TransactionServiceCardModel({@required this.id,@required this.title, this.iconPath = ""});
 
   static List<TransactionServiceCardModel> defaultData() {
     return [
-TransactionServiceCardModel(
-  title: "Reports",
-  iconPath: Icons.assessment
-  
-),
-TransactionServiceCardModel(
-  title: "Feedback",
-  iconPath:Icons.feedback_sharp
-),
-TransactionServiceCardModel(
-  title: "Assings",
-  iconPath:   Icons.report
-),
-
-TransactionServiceCardModel(
-  title:"ChainStatus",
-  iconPath: Icons.check_outlined
-),
-TransactionServiceCardModel(
-  title: "Add",
-  iconPath: Icons.add
-),
-TransactionServiceCardModel(
-  title:"Notification",
-  iconPath: Icons.notifications_active
-  ),
+      TransactionServiceCardModel(title: "Reports", iconPath: Icons.assessment,id: "report"),
+      TransactionServiceCardModel(
+          title: "Feedback", iconPath: Icons.feedback_sharp,id: "feedback"),
+      TransactionServiceCardModel(title: "Assings", iconPath: Icons.report,id: "assign"),
+      TransactionServiceCardModel(
+          title: "ChainStatus", iconPath: Icons.check_outlined,id: "chainstatus"),
+      TransactionServiceCardModel(title: "Add", iconPath: Icons.add,id: "add"),
+      TransactionServiceCardModel(
+          title: "Notification", iconPath: Icons.notifications_active,id: "notification"),
     ];
   }
 
-
-
-static List<TransactionServiceCardModel> defaultInvetoryData() {
+  static List<TransactionServiceCardModel> defaultInvetoryData() {
     return [
-TransactionServiceCardModel(
-  title: "Monitor",
-  iconPath: Icons.monitor
-  
-),
-TransactionServiceCardModel(
-  title: "Products",
-  iconPath:Icons.featured_play_list_sharp
-),
-
+      TransactionServiceCardModel(title: "Monitor", iconPath: Icons.monitor,id: "monitor"),
+      TransactionServiceCardModel(
+          title: "Products", iconPath: Icons.featured_play_list_sharp,id: "product"),
     ];
   }
-
 }
