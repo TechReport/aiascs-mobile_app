f3186bbc3a3a7e7a6a563940c293cf98e66a14fd
Kijacode <kijadanford@gmail.com>
2021-05-19 13:08:05 +0300

    refactor: analysis real data

--- lib/models/Analysing_Report_model.dart
+++ lib/models/Analysing_Report_model.dart
@@ -8,37 +8,32 @@ class AnalysingReportModel {
 
   AnalysingReportModel({this.id = "", this.title = "", this.icon, this.value});
 
-  static List<AnalysingReportModel> defaultValues() {
+  static List<AnalysingReportModel> defaultValues({@required String fake,@required String genuine ,@required String unothirized,@required String otherProduct}) {
     return [
  AnalysingReportModel(
    id: "1",
    title: "Fake Products",
    icon: Icon(Icons.linear_scale_sharp,size: 40,color: Colors.red,),
-   value: "1400"
+   value: fake
  ),
   AnalysingReportModel(
    id: "2",
    title: "Genuine Products",
    icon: Icon(Icons.linear_scale_sharp,size: 40,color: Colors.green,),
-   value: "1380"
+   value: genuine
  ),
   AnalysingReportModel(
    id: "3",
    title: "Unothirized Products",
    icon: Icon(Icons.linear_scale_sharp,size: 40,color: Colors.blue,),
-   value: "10"
+   value: unothirized
  ),
- AnalysingReportModel(
-   id: "4",
-   title: "Unknown Products",
-   icon: Icon(Icons.linear_scale_sharp,size: 40,color: Colors.amber,),
-   value: "961"
- ),
+
   AnalysingReportModel(
    id: "5",
    title: "Other Products",
    icon: Icon(Icons.linear_scale_sharp,size: 40,color: Colors.grey,),
-   value: "1400"
+   value: otherProduct
  ),
     ];
   }
