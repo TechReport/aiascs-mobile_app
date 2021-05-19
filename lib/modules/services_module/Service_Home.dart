a8362f8bd64ba9e79abf173a7d5aa4ebe5007b60
Kijacode <kijadanford@gmail.com>
2021-05-19 13:19:39 +0300

    feat: add validy visualisationreport

--- lib/modules/services_module/Service_Home.dart
+++ lib/modules/services_module/Service_Home.dart
@@ -44,177 +44,254 @@ class _ServiceHomeState extends State<ServiceHome> {
         break;
       case "product":
         onProduct();
+
         break;
       default:
         print("Here are default settings");
     }
   }
 
-  void onReport() {
+  void onReport() async {
+  
     Provider.of<AppBarTitleState>(context, listen: false)
         .setCurrentAppBarTitle("Report");
+    await Provider.of<ProductState>(context, listen: false)
+        .onVisualisationProductDetails();
+ 
     Navigator.push(context,
         new MaterialPageRoute(builder: (context) => new ReportModule()));
   }
 
   void onFeedBack() {
-      Provider.of<AppBarTitleState>(context, listen: false)
-        .setCurrentAppBarTitle("Report");
+    Provider.of<AppBarTitleState>(context, listen: false)
+        .setCurrentAppBarTitle("Feeddback");
+          Navigator.push(
+        context,
+        new MaterialPageRoute(
+            builder: (context) => new FeedBack()));
     print("on feedback");
   }
 
   void onAdd() {
-      Provider.of<AppBarTitleState>(context, listen: false)
+    Provider.of<AppBarTitleState>(context, listen: false)
         .setCurrentAppBarTitle("Report");
     print("on Add");
   }
 
   void onNotificatin() {
-      Provider.of<AppBarTitleState>(context, listen: false)
+    Provider.of<AppBarTitleState>(context, listen: false)
         .setCurrentAppBarTitle("Report");
     print("on Notification");
   }
 
   void onChainStatus() {
-      Provider.of<AppBarTitleState>(context, listen: false)
+    Provider.of<AppBarTitleState>(context, listen: false)
         .setCurrentAppBarTitle("Report");
     print("on ChainStatus");
   }
 
   void onAssigns() {
-      Provider.of<AppBarTitleState>(context, listen: false)
+    Provider.of<AppBarTitleState>(context, listen: false)
         .setCurrentAppBarTitle("Report");
     print("on Assigns");
   }
 
   void onMonitor() {
-      Provider.of<AppBarTitleState>(context, listen: false)
+    Provider.of<AppBarTitleState>(context, listen: false)
         .setCurrentAppBarTitle("Report");
     print("on monitor");
   }
+  
 
   void onProduct() {
-      Provider.of<AppBarTitleState>(context, listen: false)
+    Provider.of<AppBarTitleState>(context, listen: false)
         .setCurrentAppBarTitle("Report");
+    Provider.of<ProductState>(context, listen: false).clearState();
+
+    Provider.of<ProductState>(context, listen: false).getProductList();
+    Navigator.push(
+        context,
+        new MaterialPageRoute(
+            builder: (context) => new ProductInvetoryService()));
+
     print("on monitor");
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          InfoTopHeader(),
-          Container(
-              color: Color(0xFF264653),
-              height: MediaQuery.of(context).size.width / 9,
-              width: MediaQuery.of(context).size.width,
-              child: Row(
-                children: [
-                  Text(
-                    "Last Sync     ",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    "    4/2/2021 10:21:18 AM",
-                    style: TextStyle(
-                        color: Colors.white,
-                        wordSpacing: 4,
-                        fontSize: 12,
-                        fontWeight: FontWeight.normal),
-                  ),
-                  Text(
-                    "       Logs(3)",
-                    style: TextStyle(
-                        color: Colors.white,
-                        wordSpacing: 4,
-                        fontSize: 12,
-                        fontWeight: FontWeight.normal),
-                  )
-                ],
-              )),
-          Divider(),
-          Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-            Text("     Transactions",
-                style: TextStyle(
-                    color: Colors.grey[400],
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold)),
-          ]),
-          Material(
-            elevation: 0,
-            child: Container(
-              padding: EdgeInsets.all(13),
-              color: Colors.grey[200],
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height / 3 - 20,
-              child: GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      //maxCrossAxisExtent: 200,
-                      crossAxisCount: 4,
-                      crossAxisSpacing: 20,
-                      mainAxisSpacing: 20),
-                  itemCount: TransactionServiceCardModel.defaultData().length,
-                  itemBuilder:
-                      (BuildContext context, int transactionCardPosition) {
-                    List<TransactionServiceCardModel> cartDetails =
-                        TransactionServiceCardModel.defaultData();
-                    if (cartDetails[transactionCardPosition].title ==
-                        "Notification") {
-                      return GestureDetector(
-                        onTap: () => onNotificatin(),
-                        child: Badge(
-                          elevation: 3,
-                          badgeContent: Text(
-                            "3",
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold),
-                          ),
-                          animationDuration: Duration(microseconds: 30),
-                          badgeColor: Colors.yellow,
-                          child: Container(
-                            alignment: Alignment.center,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: [
-                                Icon(
-                                  cartDetails[transactionCardPosition].iconPath,
-                                  color: Colors.grey[400],
-                                ),
-                                Text(
-                                  cartDetails[transactionCardPosition].title,
-                                  style: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 10,
-                                  ),
-                                ),
-                              ],
+    return Consumer<LanguageState>(
+        builder: (BuildContext context, languageState, child) {
+      return Scaffold(
+        body: Column(
+          children: [
+            InfoTopHeader(),
+            Container(
+                color: Color(0xFF264653),
+                height: MediaQuery.of(context).size.width / 9,
+                width: MediaQuery.of(context).size.width,
+                child: Row(
+                  children: [
+                    Text(
+                      "Last Sync     ",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "    4/2/2021 10:21:18 AM",
+                      style: TextStyle(
+                          color: Colors.white,
+                          wordSpacing: 4,
+                          fontSize: 12,
+                          fontWeight: FontWeight.normal),
+                    ),
+                    Text(
+                      "       Logs(3)",
+                      style: TextStyle(
+                          color: Colors.white,
+                          wordSpacing: 4,
+                          fontSize: 12,
+                          fontWeight: FontWeight.normal),
+                    )
+                  ],
+                )),
+            Divider(),
+            Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+              Text("     Transactions",
+                  style: TextStyle(
+                      color: Colors.grey[400],
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold)),
+            ]),
+            Material(
+              elevation: 0,
+              child: Container(
+                padding: EdgeInsets.all(13),
+                color: Colors.grey[200],
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height / 3 - 20,
+                child: GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        //maxCrossAxisExtent: 200,
+                        crossAxisCount: 4,
+                        crossAxisSpacing: 20,
+                        mainAxisSpacing: 20),
+                    itemCount: TransactionServiceCardModel.defaultData().length,
+                    itemBuilder:
+                        (BuildContext context, int transactionCardPosition) {
+                      List<TransactionServiceCardModel> cartDetails =
+                          TransactionServiceCardModel.defaultData();
+                      if (cartDetails[transactionCardPosition].title ==
+                          "Notification") {
+                        return GestureDetector(
+                          onTap: () => onNotificatin(),
+                          child: Badge(
+                            elevation: 3,
+                            badgeContent: Text(
+                              "3",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold),
                             ),
-                            decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(12)),
+                            animationDuration: Duration(microseconds: 30),
+                            badgeColor: Colors.yellow,
+                            child: Container(
+                              alignment: Alignment.center,
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  Icon(
+                                    cartDetails[transactionCardPosition]
+                                        .iconPath,
+                                    color: Colors.grey[400],
+                                  ),
+                                  Text(
+                                    cartDetails[transactionCardPosition].title,
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 10,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(12)),
+                            ),
                           ),
+                        );
+                      }
+                      return GestureDetector(
+                        onTap: () => onTransaction(
+                            cartDetails[transactionCardPosition].id),
+                        child: Container(
+                          alignment: Alignment.center,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Icon(
+                                cartDetails[transactionCardPosition].iconPath,
+                                color: Colors.grey[400],
+                              ),
+                              Text(
+                                cartDetails[transactionCardPosition].title,
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 10,
+                                ),
+                              ),
+                            ],
+                          ),
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(12)),
                         ),
                       );
-                    }
+                    }),
+              ),
+            ),
+            Divider(),
+            Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+              Text("     Invetory",
+                  style: TextStyle(
+                      color: Colors.grey[400],
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold)),
+            ]),
+            Divider(
+              height: MediaQuery.of(context).size.height / 15,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 10,
+              color: Colors.transparent,
+              child: ListView.builder(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemExtent: 90,
+                  // padding: EdgeInsets.all(3),
+                  itemCount:
+                      TransactionServiceCardModel.defaultInvetoryData().length,
+                  itemBuilder: (BuildContext context, int invetoryPostion) {
+                    List<TransactionServiceCardModel> inveteryDetails =
+                        TransactionServiceCardModel.defaultInvetoryData();
                     return GestureDetector(
-                      onTap: () => onTransaction(
-                          cartDetails[transactionCardPosition].id),
+                      onTap: () =>
+                          onTransaction(inveteryDetails[invetoryPostion].id),
                       child: Container(
+                        margin: EdgeInsets.only(left: 20),
                         alignment: Alignment.center,
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.spaceAround,
                           children: [
                             Icon(
-                              cartDetails[transactionCardPosition].iconPath,
+                              inveteryDetails[invetoryPostion].iconPath,
                               color: Colors.grey[400],
                             ),
                             Text(
-                              cartDetails[transactionCardPosition].title,
+                              inveteryDetails[invetoryPostion].title,
                               style: TextStyle(
                                 color: Colors.black,
                                 fontSize: 10,
@@ -228,64 +305,10 @@ class _ServiceHomeState extends State<ServiceHome> {
                       ),
                     );
                   }),
-            ),
-          ),
-          Divider(),
-          Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-            Text("     Invetory",
-                style: TextStyle(
-                    color: Colors.grey[400],
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold)),
-          ]),
-          Divider(
-            height: MediaQuery.of(context).size.height / 15,
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height / 10,
-            color: Colors.transparent,
-            child: ListView.builder(
-                shrinkWrap: true,
-                scrollDirection: Axis.horizontal,
-                itemExtent: 90,
-                // padding: EdgeInsets.all(3),
-                itemCount:
-                    TransactionServiceCardModel.defaultInvetoryData().length,
-                itemBuilder: (BuildContext context, int invetoryPostion) {
-                  List<TransactionServiceCardModel> inveteryDetails =
-                      TransactionServiceCardModel.defaultInvetoryData();
-                  return GestureDetector(
-                    onTap: () =>
-                        onTransaction(inveteryDetails[invetoryPostion].id),
-                    child: Container(
-                      margin: EdgeInsets.only(left: 20),
-                      alignment: Alignment.center,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Icon(
-                            inveteryDetails[invetoryPostion].iconPath,
-                            color: Colors.grey[400],
-                          ),
-                          Text(
-                            inveteryDetails[invetoryPostion].title,
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 10,
-                            ),
-                          ),
-                        ],
-                      ),
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(12)),
-                    ),
-                  );
-                }),
-          )
-        ],
-      ),
-    );
+            )
+          ],
+        ),
+      );
+    });
   }
 }
--- lib/modules/services_module/component/Visualisation_analysing_report.dart
+++ lib/modules/services_module/component/Visualisation_analysing_report.dart
@@ -1,19 +1,30 @@
+import 'package:aiascs_mobile/app_state/product_state.dart';
 import 'package:aiascs_mobile/models/Analysing_Report_model.dart';
 import 'package:aiascs_mobile/modules/services_module/reports/components/Visualisation_analysing_body.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class VisualisationAnallysingReport extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: AnalysingReportModel.defaultValues().length,
-        itemBuilder: (context, int analysingReportModelIndex) {
-          AnalysingReportModel analysingReport =  AnalysingReportModel.defaultValues()[analysingReportModelIndex];
+    return Consumer<ProductState>(
+        builder: (BuildContext context, productState, child) {
+      return Column(
+        children: AnalysingReportModel.defaultValues(
+          fake: productState.countFakeProduct.toString(),
+          genuine: productState.countGenuineProduct.toString(),
+          otherProduct: (productState.countAllProductProduct - (productState.countFakeProduct +productState.countGenuineProduct)).toString(),
+          unothirized: productState.countAllProductProduct.toString()
+        )
+            .map((AnalysingReportModel analysingReport) {
           return VisualisationAnalysingBody(
             title: analysingReport.title,
             visualValue: analysingReport.value,
             visualIcon: analysingReport.icon,
           );
-        });
+        }).toList(),
+      );
+    });
+
   }
 }
