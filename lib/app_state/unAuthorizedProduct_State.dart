4beeba5c3c24e1051b9cf20f37775d9b0cebedb7
Kijacode <kijadanford@gmail.com>
2021-06-03 11:13:26 +0300

    feat: intergrate unauthorized product state

--- lib/app_state/unAuthorizedProduct_State.dart
+++ lib/app_state/unAuthorizedProduct_State.dart
@@ -1,4 +1,5 @@
 import 'package:aiascs_mobile/core/services/unauthorized_product_serivce/unauthorized_product_service.dart';
+import 'package:aiascs_mobile/models/UnAuthorized_Product.dart';
 import 'package:flutter/foundation.dart';
 import 'dart:io';
 
@@ -7,18 +8,36 @@ class UnAuthorizedProductState extends ChangeNotifier {
 //init state
 
   bool _isLoading;
+  List<UnAuthorizedProduct> _listUnAuthProduct = [];
+  UnAuthorizedProduct _currentUnAuthorizedProduct;
 
 //selector
   bool get isLoading => _isLoading ?? false;
+  List<UnAuthorizedProduct> get listUnAuthProduct => _listUnAuthProduct ?? [];
+  UnAuthorizedProduct get currentUnAuthiredProduct =>
+      _currentUnAuthorizedProduct;
 //reducer
 
-  Future<void> onPostProduct(String photoPath, String companyName, String description,
-      String productName) async {
+  Future<void> onPostProduct(String photoPath, String companyName,
+      String description, String productName) async {
     _isLoading = true;
     notifyListeners();
     await UnAuthorizedProductService().postUnAuthorizedProduct(
         photoPath, companyName, description, productName);
     _isLoading = false;
+    notifyListeners();
+  }
+
+  Future onRetrieveUnAuthroduct() async {
+    _listUnAuthProduct =
+        await UnAuthorizedProductService().onRetrieveUnAuthPrdoctListOffline();
+    print("data in lists");
+    print(_listUnAuthProduct.length);
+    notifyListeners();
+  }
+
+  onSetCurrentAuthirizedProduct(UnAuthorizedProduct authorizedProduct) {
+    _currentUnAuthorizedProduct = authorizedProduct;
     notifyListeners();
   }
 }
